162d9b5c9390a8153822bd086bf38d5bc9e54c71
Ivo Dilov <iddilov@gmail.com>
2020-02-14 23:11:34 +0200

    sample

--- sample.py
+++ sample.py
@@ -1,8 +1,26 @@
+import xml.etree.ElementTree as ET
 from tfidf.query import query
+from src_qa.qa import QA
 
-docs = query('Машинно самообучение', 3)
-print()
-for doc in docs:
-    print(doc['title'])
-    print(doc['url'])
-    print()
+class AnswerQuestion:
+    def __init__(self, corpus = 'bgwiki-20180920-corpus.xml'): # https://dumps.wikimedia.org/bgwiki/20200201/bgwiki-20200201-pages-articles.xml.bz2
+        self.QA = QA()
+        self.root = ET.parse(corpus).getroot()
+
+    def query(self, question, n_docs=10, n_answers=10):
+        docs = query(question, n_docs)
+        article_ids = [doc['id'] for doc in docs]
+        articles = [self.root.find(f"./article[@id={idx}]") for idx in article_ids]
+        answers = self.QA.query_adv_emb_match(question, articles, None, n_answers)
+        return [a[0] for a in answers]
+
+aq = AnswerQuestion()
+ans = aq.query("Кога е започнала Втората Световна Война")
+for a in ans:
+    print(a)
+# docs = query('Машинно самообучение', 3)
+# print()
+# for doc in docs:
+#     print(doc['title'])
+#     print(doc['url'])
+#     print()
